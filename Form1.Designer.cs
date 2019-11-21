97bda218f8f1488fb39d894b25fa6bbc8eb8f9b6
gaowq <1138946467@qq.com>
2019-11-21 19:03:53 +0800

    主要功能

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace AutoSaveOrRemind
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(70, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(70, 188);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "关闭";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(70, 102);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 21);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "60";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(165, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "分钟";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(70, 42);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(71, 16);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "自动保存";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Click += new System.EventHandler(this.radioButton1_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(70, 65);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 16);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.Text = "提醒";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Click += new System.EventHandler(this.radioButton2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 450);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "AutoSaveOrRemind";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoSaveOrRemind
+{
+    //todo 1.ps,ai,id
+    //
+
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        private static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+        private const byte vbKeyControl = 0x11;   // CTRL 键
+        private const byte vbKeyS = 83;
+        //提醒和自动保存线程
+        private Thread thread;
+        //计算使用时间线程
+        private Thread thread2;
+
+        //自动保存默认时间：1h
+        private int circleTime = 60 * 60 * 1000;
+        private int gapTime = 1000;
+        private int countGap = 0;
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("User32.dll")]
+        static extern int GetWindowText(IntPtr handle, StringBuilder text, int MaxLen);
+        [DllImport("user32.dll")]
+        private static extern int GetWindowTextLength(IntPtr hWnd);
+
+        private string[] autoSoftName = { "abc.exe" };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 离开事件
+        /// </summary>
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StopAuto();
+        }
+
+        /// <summary>
+        /// 开始
+        /// </summary>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //先退出当前线程
+            StopAuto();
+
+            if (radioButton1.Checked)
+            {
+                thread = new Thread(new ThreadStart(AutoPressSave));
+                thread.Start();
+            }
+            else
+            {
+                thread = new Thread(new ThreadStart(AutoRemind));
+                thread.Start();
+            }
+            MessageBox.Show("设置成功");
+        }
+
+        /// <summary>
+        /// 关闭
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StopAuto();
+            MessageBox.Show("关闭成功");
+        }
+
+        #region 自动方法
+        /// <summary>
+        /// 自动按键
+        /// </summary>
+        private void AutoPressSave()
+        {
+            while (true)
+            {
+                //按下
+                keybd_event(vbKeyControl, 0, 0, 0);
+                keybd_event(vbKeyS, 0, 0, 0);
+                Thread.Sleep(100);
+                //松开
+                keybd_event(vbKeyControl, 0, 2, 0);
+                keybd_event(vbKeyS, 0, 2, 0);
+                Thread.Sleep(circleTime);
+            }
+        }
+
+        /// <summary>
+        /// 自动提醒
+        /// </summary>
+        private void AutoRemind()
+        {
+            while (true)
+            {
+                Thread.Sleep(circleTime);
+                MessageBox.Show((circleTime / 60000).ToString() + "分钟过去了，该保存了");
+            }
+        }
+
+        private void StopAuto()
+        {
+            if (thread != null)
+            {
+                thread.Abort();
+            }
+        }
+
+
+        private void GapTimeCount()
+        {
+            while (true)
+            {
+                Thread.Sleep(gapTime);
+                if(autoSoftName.Contains(GetCurrentSoft()))
+                    countGap++;
+            }
+        }
+
+        private string GetCurrentSoft()
+        {
+            var hand = GetForegroundWindow();
+            int len = GetWindowTextLength(hand);
+            StringBuilder text = new StringBuilder(len + 1);
+            int i = GetWindowText(hand, text, len + 1);
+            return text.ToString().Trim();
+        }
+        #endregion
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                int newCircleTime = 0;
+                int.TryParse(textBox1.Text, out newCircleTime);
+
+                if (newCircleTime > 0)
+                {
+                    circleTime = newCircleTime * 60 * 1000;
+                }
+            }
+        }
+
+        private void radioButton1_Click(object sender, EventArgs e)
+        {
+            radioButton1.Checked = true;
+            radioButton2.Checked = false;
+        }
+
+        private void radioButton2_Click(object sender, EventArgs e)
+        {
+            radioButton1.Checked = false;
+            radioButton2.Checked = true;
+
+
+        }
+    }
+}
